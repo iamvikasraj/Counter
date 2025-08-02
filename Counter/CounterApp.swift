3305230fff76455b05f644d27f7ac8b86ea84d15
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2025-08-02 11:56:42 +0530

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Vikas Raj Yadav on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
